905adadcd5132f0a0964a594646b483fc62aab0d
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-30 17:22:39 +0530

    make task components

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,24 +1,24 @@
-import {Task} from './Task';
+import { Task } from './Task';
 
-export const TASKS:Task[]= [
-{
-id:1,
-text:"Doctors Appointment",
-day:'May 5th at 2:30pm ',
-reminder:true,
-},
-{
-  id:2,
-  text:"Meeting to School ",
-  day:'May 6th at 1:30pm ',
-  reminder:true,
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: 'May 5th at 2:30pm ',
+    reminder: true,
   },
   {
-    id:3,
-    text:"Food Shoping ",
-    day:'May 5th at 12:30pm ',
-    reminder:true,
-    }
+    id: 2,
+    text: "Meeting to School ",
+    day: 'May 6th at 1:30pm ',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shoping ",
+    day: 'May 5th at 12:30pm ',
+    reminder: true,
+  }
 ]
 
 
