fe0ddd93bd5efe70d3202772f199a5f5c7c2824e
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-30 14:49:13 +0530

    task tracker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'Time Tracker App';
+  title:string = 'Task Tracker App';
 }
