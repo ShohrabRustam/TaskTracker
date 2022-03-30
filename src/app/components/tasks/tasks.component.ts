4cc6e41533b4fa83e1f0448ca868ad224352a4b9
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-30 17:46:43 +0530

    send the task text to the task component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../mock-tasks'
-import {Task} from '../../Task'
+import { TASKS } from '../../mock-tasks';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
