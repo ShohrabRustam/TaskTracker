016c5e7414716ac00a905b1044bc2fe84760be8f
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-31 09:48:23 +0530

    http in task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 
+// for the Task
 import { Task } from '../Task';
+
+// task data
 import { TASKS } from '../mock-tasks';
 import {Observable,of} from 'rxjs'
 
