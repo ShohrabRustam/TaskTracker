3d2028336f0e5d1083f355c22c556370ff07d0a0
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-31 10:08:45 +0530

    setup the service to get data from fake db

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -5,7 +5,7 @@ import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { Task } from '../Task';
 
 // task data
-import { TASKS } from '../mock-tasks';
+// import { TASKS } from '../mock-tasks';
 import {Observable,of} from 'rxjs'
 
 @Injectable({
@@ -14,9 +14,9 @@ import {Observable,of} from 'rxjs'
 export class TaskService {
 
   private apiUrl = "http://localhost:3000/tasks"
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getTask():Observable<Task[]>{
+  getTask(): Observable<Task[]>{
     // const task = of(TASKS);
     // return task;
     return this.http.get<Task[]>(this.apiUrl);
